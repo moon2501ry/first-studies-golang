20726de161c8a3d22507bb9cb1c2bcc0a71c459b
Ryan A. O. <159044164+moon2501ry@users.noreply.github.com>
2026-01-07 11:46:49 -0300

    Refactor code structure for improved readability and maintainability

--- test-fractal/fractal.go
+++ test-fractal/fractal.go
@@ -18,7 +18,15 @@ func main() {
 	var iteracoes int
 	fmt.Scanf("%d\n", &iteracoes)
 
-	for i := 1; i >= iteracoes; i++ {
-		math.Cos((float32(i) * angulo))
+	fmt.Print("Valor de redução: ")
+	var reduc float32
+	fmt.Scanf("%f\n", &reduc)
+
+	var max_v float64 = 0
+	var max_h float64 = 0
+	for i := 1; i <= iteracoes; i++ {
+		max_v += math.Abs(math.Cos(float64(float32(i)*angulo)/2)) * float64(a/(reduc*float32(i)))
+		max_h += math.Abs(math.Sin(float64(float32(i)*angulo)/2)) * float64(a/(reduc*float32(i)))
 	}
+	fmt.Printf("Crescimento Máximo Vertical: %f\nCrescimento Máximo Horizontal: %f\nAltura Máxima: %f\nLagura Máxima: %f\n", max_v, max_h, max_v+float64(a), max_h*2)
 }
